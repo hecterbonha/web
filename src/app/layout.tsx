8ee7ad9dd8d34d50bb21df8107f0eac2eb23acae
hecterbonha <hecterbonha@gmail.com>
2024-11-21 20:25:35 +0000

    ok

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,39 @@
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import type { NextFontWithVariable } from "next/dist/compiled/@next/font";
+import type {NextFontWithVariable} from "next/dist/compiled/@next/font";
 
 const geistSans: NextFontWithVariable = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+    src: "./fonts/GeistVF.woff",
+    variable: "--font-geist-sans",
+    weight: "100 900",
 });
-const geistMono:  NextFontWithVariable= localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const geistMono: NextFontWithVariable = localFont({
+    src: "./fonts/GeistMonoVF.woff",
+    variable: "--font-geist-mono",
+    weight: "100 900",
 });
 
 export const metadata: Metadata = {
-  title: "Malah Ngoding | Lupa Makan, Lupa Tidur, Malah Ngoding",
-  description:
-    "Malah Ngoding adalah platform belajar pemrograman yang menyediakan tutorial, artikel, dan contoh proyek untuk membantu developer mengembangkan keterampilan coding mereka.",
+    title: "Malah Ngoding | Lupa Makan, Lupa Tidur, Malah Ngoding",
+    description:
+        "Malah Ngoding adalah platform belajar pemrograman yang menyediakan tutorial, artikel, dan contoh proyek untuk membantu developer mengembangkan keterampilan coding mereka.",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-      <nav>This is the navigation bar</nav>
+    return (
+        <html lang="en">
+        <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+        <nav>This is the navigation bar</nav>
         {children}
         <footer>This will be footer</footer>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
