e17f328c65704aac45a4ea32481ba34723fc0d66
hecterbonha <hecterbonha@gmail.com>
2024-11-22 01:55:05 +0000

    ok

--- src/app/api/healthcheck/route.ts
+++ src/app/api/healthcheck/route.ts
@@ -1,7 +1,7 @@
 // export const dynamic = 'force-static'
 
 export async function GET() {
-    const response = await fetch('https://api.github.com/')
-    const data = await response.json()
-    return Response.json({data})
+  const response = await fetch('https://api.github.com/');
+  const data = await response.json();
+  return Response.json({ data });
 }
